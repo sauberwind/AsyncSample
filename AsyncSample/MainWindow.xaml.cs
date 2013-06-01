0c743edebbb2ad359ef52e602dd0974f438fefcd
Hal(@sakuraSoftware) <sauber@pluto.dti.ne.jp>
2013-06-02 00:19:10 +0900

    Asyncサンプル

---
+++ AsyncSample/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+
+namespace AsyncSample
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            status.Text = "1回目";
+            await Task.Run(() => Thread.Sleep(1000));
+            status.Text = "2回目";
+            await Task.Run(() => Thread.Sleep(1000));
+            status.Text = "3回目";
+        }
+    }
+}
